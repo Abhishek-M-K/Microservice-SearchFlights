d7380dd53dd06bb6d3882e49fe4c6998d6043025
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-02 17:30:44 +0530

    chore : add airport model

--- src/models/airport.js
+++ src/models/airport.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -9,19 +9,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Airport, {
+      this.belongsTo(models.City, {
         foreignKey: "cityId",
+        onDelete: "CASCADE", // if a city is deleted, delete all airports in that city
       });
     }
   }
-  City.init(
+  Airport.init(
     {
-      name: { type: DataTypes.STRING, allowNull: false, unique: true },
+      name: { type: DataTypes.STRING, allowNull: false },
+      address: DataTypes.STRING,
+      cityId: { type: DataTypes.INTEGER, allowNull: false },
     },
     {
       sequelize,
-      modelName: "City",
+      modelName: "Airport",
     }
   );
-  return City;
+  return Airport;
 };
