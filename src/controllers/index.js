2be8a32ad74858bdf190e9069b14c7613db47e5b
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-07 14:55:10 +0530

    refactor : cleanup

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CityController: require("./city-controller"),
+  FlightController: require("./flight-controller"),
+  AirportController: require("./airport-controller"),
+};
--- src/repository/index.js
+++ src/repository/index.js
@@ -2,4 +2,6 @@ module.exports = {
   CityRepository: require("./city-repository"),
   AirplaneRepository: require("./airplane-repository"),
   FlightRepository: require("./flight-repository"),
+  AirportRepository: require("./airport-repository"),
+  CrudRepository: require("./crud-repository"),
 };
